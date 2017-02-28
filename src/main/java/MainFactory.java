e4d24ed32b421985df4048d96a293ca1e345791c
Zaytsev Maxim <maxim.zaytsev@technavology.com>
2017-02-28 17:42:03 +0300

    add class of Main factory

---
+++ src/main/java/MainFactory.java
@@ -0,0 +1,22 @@
+import engine.EnginesControlFactory;
+
+public class MainFactory {
+
+    public static final MainFactory INSTANCE = new MainFactory();
+
+    private EnginesControlFactory enginesControlFactory;
+
+    private MainFactory() {
+    }
+
+    public EnginesControlFactory getEnginesControlFactory() {
+        if (enginesControlFactory == null) {
+            throw new RuntimeException("Main factory is not configured!");
+        }
+        return enginesControlFactory;
+    }
+
+    public void setEnginesControlFactory(EnginesControlFactory enginesControlFactory) {
+        this.enginesControlFactory = enginesControlFactory;
+    }
+}
