30e0166ab0da329e59c662b98fd481379cc0333f
Zaytsev Maxim <maxim.zaytsev@technavology.com>
2017-03-01 15:12:34 +0300

    test for main factory

--- src/test/java/factory/MainFactoryTest.java
+++ src/test/java/factory/MainFactoryTest.java
@@ -2,19 +2,22 @@ package factory;
 
 import engine.EnginesControlFactory;
 import org.junit.Test;
-import sun.applet.Main;
+
+import static org.junit.Assert.assertEquals;
 
 public class MainFactoryTest {
 
     @Test
     public void setEnginesTest() {
-        EnginesControlFactory actual = ()->null;
+        EnginesControlFactory actual = () -> null;
         MainFactory.INSTANCE.setEnginesControlFactory(actual);
         EnginesControlFactory expected = MainFactory.INSTANCE.getEnginesControlFactory();
+        assertEquals(expected, actual);
     }
 
     @Test(expected = RuntimeException.class)
     public void getNonConfiguredTest() {
+        MainFactory.INSTANCE.setEnginesControlFactory(null);
         MainFactory.INSTANCE.getEnginesControlFactory();
     }
 
