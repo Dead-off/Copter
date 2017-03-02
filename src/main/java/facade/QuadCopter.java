a44f0708a5aa6e22dd8c63842a5613a29c8f93fc
Zaytsev Maxim <maxim.zaytsev@technavology.com>
2017-03-02 15:09:44 +0300

    impl for quadrocopter class

--- src/main/java/facade/QuadCopter.java
+++ src/main/java/facade/QuadCopter.java
@@ -1,20 +1,26 @@
 package facade;
 
 import engine.CopterController;
+import engine.PowerCalculator;
+import engine.QuadEnginePowerContainer;
+import engine.QuadroPowerCalculator;
 import factory.MainFactory;
 import proto.CopterDirection;
 
 public class QuadCopter implements Copter {
 
     private final CopterController copterController;
+    private final PowerCalculator powerCalculator;
 
     public QuadCopter() {
         this.copterController = MainFactory.INSTANCE.getEnginesControlFactory().getCopterController();
+        this.powerCalculator = new QuadroPowerCalculator();
     }
 
     @Override
     public void handleDirectionChange(CopterDirection.Direction newDirection) {
-
+        QuadEnginePowerContainer calculatePower = powerCalculator.calculateEnginesPower(newDirection);
+        copterController.setEnginesPower(calculatePower);
     }
 
     @Override
