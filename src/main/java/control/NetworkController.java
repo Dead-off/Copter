cc6cdbe1becb9b2e692e58f1b262809ff443ab6f
Zaytsev Maxim <maxim.zaytsev@technavology.com>
2017-03-07 17:29:57 +0300

    add net user controller implementation with tests

--- src/main/java/control/NetworkController.java
+++ src/main/java/control/NetworkController.java
@@ -1,14 +1,62 @@
 package control;
 
 import facade.Copter;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.bytes.ByteArrayDecoder;
+import io.netty.handler.codec.bytes.ByteArrayEncoder;
+import proto.CopterDirection;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class NetworkController implements UserController {
 
     private Copter copter;
+    private final int port;
+
+    private volatile ServerCallback onServerStart;
+    private volatile ServerCallback onChannelRead;
+
+    public NetworkController(int port) {
+        this.port = port;
+    }
 
     @Override
     public void run() {
-        // TODO: 02.03.17 impl
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new ByteArrayEncoder());
+                            ch.pipeline().addLast(new ByteArrayDecoder());
+                            ch.pipeline().addLast(new Handler());
+                        }
+                    })
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true);
+
+            ChannelFuture f = b.bind(port).sync();
+            ServerCallback onServerStartScope = this.onServerStart;
+            if (onServerStartScope != null) {
+                onServerStartScope.handle();
+            }
+            f.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            System.out.println("server stopped!");
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
     }
 
     @Override
@@ -19,5 +67,51 @@ public class NetworkController implements UserController {
     @Override
     public Copter getCopter() {
         return copter;
+    }
+
+    void setOnServerStart(ServerCallback onServerStart) {
+        this.onServerStart = onServerStart;
+    }
+
+    void setOnChannelRead(ServerCallback onChannelRead) {
+        this.onChannelRead = onChannelRead;
+    }
+
+
+    private class Handler extends SimpleChannelInboundHandler<byte[]> {
+
+        private final List<Byte> bytes = new ArrayList<>();
+
+        @Override
+        protected void channelRead0(ChannelHandlerContext ctx, byte[] msg) throws Exception {
+            for (byte b : msg) {
+                bytes.add(b);
+            }
+        }
+
+        @Override
+        public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+            byte[] bytesArr = new byte[bytes.size()];
+            for (int i = 0; i < bytes.size(); i++) {
+                bytesArr[i] = bytes.get(i);
+            }
+            copter.handleDirectionChange(CopterDirection.Direction.parseFrom(bytesArr));
+            ServerCallback channelRead = NetworkController.this.onChannelRead;
+            if (channelRead != null) {
+                channelRead.handle();
+            }
+        }
+
+        @Override
+        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+            cause.printStackTrace();
+            ctx.close();
+        }
+    }
+
+    public interface ServerCallback {
+
+        void handle();
+
     }
 }
---
+++ src/test/java/control/NetworkControllerTest.java
@@ -0,0 +1,64 @@
+package control;
+
+import facade.Copter;
+import org.junit.Test;
+import proto.CopterDirection;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.concurrent.atomic.AtomicReference;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class NetworkControllerTest {
+
+    @Test
+    public void runTest() {
+        NetworkController controller = new NetworkController(15127);
+        final AtomicReference<CopterDirection.Direction> directionReference = new AtomicReference<>();
+        controller.setCopter(new Copter() {
+            @Override
+            public void handleDirectionChange(CopterDirection.Direction newDirection) {
+                directionReference.set(newDirection);
+            }
+
+            @Override
+            public void clientConnectionLost() {
+
+            }
+        });
+        Thread serverThread = new Thread(controller::run);
+        CopterDirection.Direction expected = CopterDirection.Direction.newBuilder()
+                .setForward(1)
+                .setLeft(0.3)
+                .setRotateLeft(0.7)
+                .setPower(0.8).build();
+        serverThread.start();
+        final Thread currentThread = Thread.currentThread();
+        controller.setOnChannelRead(currentThread::interrupt);
+        controller.setOnServerStart(currentThread::interrupt);
+        try {
+            Thread.sleep(1000);
+            fail();
+        } catch (InterruptedException ignored) {
+        }
+        try (Socket socket = new Socket("127.0.0.1", 15127);
+             OutputStream os = socket.getOutputStream();) {
+            os.write(expected.toByteArray());
+            os.flush();
+            try {
+                Thread.sleep(1000);
+                fail();
+            } catch (InterruptedException ignored) {
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            fail();
+        }
+        serverThread.interrupt();
+        CopterDirection.Direction actual = directionReference.get();
+        assertEquals(expected, actual);
+    }
+}
