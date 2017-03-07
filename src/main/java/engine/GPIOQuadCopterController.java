836ac292d62ea5986082887d17d0dab2d437aa5b
Zaytsev Maxim <maxim.zaytsev@technavology.com>
2017-03-07 18:07:13 +0300

    template for gpio test and gpio controller

--- src/main/java/engine/GPIOQuadCopterController.java
+++ src/main/java/engine/GPIOQuadCopterController.java
@@ -1,20 +1,37 @@
 package engine;
 
 import com.pi4j.io.gpio.*;
+import com.pi4j.wiringpi.Gpio;
+
+import static com.pi4j.wiringpi.Gpio.PWM_MODE_MS;
+import static com.pi4j.wiringpi.Gpio.PWM_OUTPUT;
 
 public class GPIOQuadCopterController implements CopterController {
 
-    //todo implementation
-//    пока не очень ясно, как будут работать esc. Возможно потребуется ещё что-то типа GPIOWriter
-//    который будет запускаться в отдельном потоке и переключать пины с заданными интервалом
-//    можно тогда добавить package-private getter , который будет отдавать текущее состояние мотора (крутится/нет)
-//    и юнитом это тестировать. То есть засетили - проверили, подождали N секунд - проверили, подождали ещё - проверили
+    private final GpioController GPIO;
+    private final GpioPinPwmOutput leftFrontEngine;
+    private final GpioPinPwmOutput leftBackEngine;
+    private final GpioPinPwmOutput rightFrontEngine;
+    private final GpioPinPwmOutput rightBackEngine;
+
+    GPIOQuadCopterController(GpioController GPIO) {
+        this.GPIO = GPIO;
+        this.leftFrontEngine = GPIO.provisionPwmOutputPin(RaspiPin.GPIO_26);
+        this.leftBackEngine = GPIO.provisionPwmOutputPin(RaspiPin.GPIO_26);
+        //todo check can use so gpio pins, set ports, set props from t() func
+        this.rightFrontEngine = GPIO.provisionPwmOutputPin(RaspiPin.GPIO_26);
+        this.rightBackEngine = GPIO.provisionPwmOutputPin(RaspiPin.GPIO_26);
+    }
+
+    public GPIOQuadCopterController() {
+        this(GpioFactory.getInstance());
+    }
 
     @Override
     public void setEnginesPower(QuadEnginePowerContainer powerContainer) {
-        final GpioController gpio = GpioFactory.getInstance();
 
-        final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00, "MyLED", PinState.HIGH);
+
+        final GpioPinDigitalOutput pin = GPIO.provisionDigitalOutputPin(RaspiPin.GPIO_00, "MyLED", PinState.HIGH);
         System.out.println("on");
         pin.setShutdownOptions(true, PinState.LOW);
         try {
@@ -23,11 +40,28 @@ public class GPIOQuadCopterController implements CopterController {
 
         }
         pin.low();
-        gpio.shutdown();
+        GPIO.shutdown();
+    }
+
+    public void t() {
+//        pin0.setPwmRange();
+//        GpioFactory.getDefaultProvider().setP
+//        Gpio.pinMode(26, PWM_OUTPUT);
+//        Gpio.pwmSetMode(PWM_MODE_MS);
+//        Gpio.pwmSetClock(384);//19.2e6/(clock*range) max value of clock*range == 2^24 (4096*4096)
+//        Gpio.pwmSetRange(1000);//value==range - output 1, value == 0 output 0
+//        int value = 50+(int)Math.round(powerContainer.getLeftBackEnginePower()*50);
+//        System.out.println(value);
+//        System.out.println("pulse on 50hz " + (value/1000));
+//        Gpio.pwmWrite(26, value);
+////        pin
+////        pin0.setPwm((int)Math.round(1024*powerContainer.getLeftBackEnginePower()));
     }
 
     @Override
     public void close() {
-
+        GPIO.shutdown();
+        //todo release
+//        GPIO.unprovisionPin(pin0);
     }
 }
---
+++ src/test/java/engine/GPIOQuadCopterControllerTest.java
@@ -0,0 +1,13 @@
+package engine;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class GPIOQuadCopterControllerTest {
+
+    @Test
+    public void setEnginesPowerTest() {
+        assertEquals(1,1);
+    }
+}
