43ddf3b3fa4979e3238be41e158443ee585e4708
Dead_off <AnterKan1@gmail.com>
2017-03-07 20:18:27 +0300

    Gpio controller impl

--- src/main/java/engine/GPIOQuadCopterController.java
+++ src/main/java/engine/GPIOQuadCopterController.java
@@ -4,64 +4,58 @@ import com.pi4j.io.gpio.*;
 import com.pi4j.wiringpi.Gpio;
 
 import static com.pi4j.wiringpi.Gpio.PWM_MODE_MS;
-import static com.pi4j.wiringpi.Gpio.PWM_OUTPUT;
 
 public class GPIOQuadCopterController implements CopterController {
 
+    private final static int DEFAULT_RANGE = 1000;
+
     private final GpioController GPIO;
-    private final GpioPinPwmOutput leftFrontEngine;
-    private final GpioPinPwmOutput leftBackEngine;
-    private final GpioPinPwmOutput rightFrontEngine;
-    private final GpioPinPwmOutput rightBackEngine;
+    private final GpioPinPwmOutput leftFrontPin;
+    private final GpioPinPwmOutput leftBackPin;
+    private final GpioPinPwmOutput rightFrontPin;
+    private final GpioPinPwmOutput rightBackPin;
 
     GPIOQuadCopterController(GpioController GPIO) {
         this.GPIO = GPIO;
-        this.leftFrontEngine = GPIO.provisionPwmOutputPin(RaspiPin.GPIO_26);
-        this.leftBackEngine = GPIO.provisionPwmOutputPin(RaspiPin.GPIO_26);
-        //todo check can use so gpio pins, set ports, set props from t() func
-        this.rightFrontEngine = GPIO.provisionPwmOutputPin(RaspiPin.GPIO_26);
-        this.rightBackEngine = GPIO.provisionPwmOutputPin(RaspiPin.GPIO_26);
+        this.leftFrontPin = GPIO.provisionPwmOutputPin(RaspiPin.GPIO_26);//GPIO 32 + 34 ground
+        this.leftBackPin = GPIO.provisionPwmOutputPin(RaspiPin.GPIO_21);//GPIO 29 + 30 ground
+        this.rightFrontPin = GPIO.provisionPwmOutputPin(RaspiPin.GPIO_25);//GPIO 37 + 39 Ground
+        this.rightBackPin = GPIO.provisionPwmOutputPin(RaspiPin.GPIO_00);//GPIO 11 + 9 Ground
+
+        init();
     }
 
     public GPIOQuadCopterController() {
         this(GpioFactory.getInstance());
     }
 
-    @Override
-    public void setEnginesPower(QuadEnginePowerContainer powerContainer) {
-
-
-        final GpioPinDigitalOutput pin = GPIO.provisionDigitalOutputPin(RaspiPin.GPIO_00, "MyLED", PinState.HIGH);
-        System.out.println("on");
-        pin.setShutdownOptions(true, PinState.LOW);
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-
-        }
-        pin.low();
-        GPIO.shutdown();
+    private void init() {
+        Gpio.pwmSetMode(PWM_MODE_MS);
+        this.leftBackPin.setMode(PinMode.PWM_OUTPUT);
+        this.leftFrontPin.setMode(PinMode.PWM_OUTPUT);
+        this.rightFrontPin.setMode(PinMode.PWM_OUTPUT);
+        this.rightBackPin.setMode(PinMode.PWM_OUTPUT);
+        this.leftFrontPin.setPwmRange(DEFAULT_RANGE);
+        this.leftBackPin.setPwmRange(DEFAULT_RANGE);
+        this.rightFrontPin.setPwmRange(DEFAULT_RANGE);
+        this.rightBackPin.setPwmRange(DEFAULT_RANGE);
+        Gpio.pwmSetClock(384);//19.2e6/(clock*range) - hz pwm output
     }
 
-    public void t() {
-//        pin0.setPwmRange();
-//        GpioFactory.getDefaultProvider().setP
-//        Gpio.pinMode(26, PWM_OUTPUT);
-//        Gpio.pwmSetMode(PWM_MODE_MS);
-//        Gpio.pwmSetClock(384);//19.2e6/(clock*range) max value of clock*range == 2^24 (4096*4096)
-//        Gpio.pwmSetRange(1000);//value==range - output 1, value == 0 output 0
-//        int value = 50+(int)Math.round(powerContainer.getLeftBackEnginePower()*50);
-//        System.out.println(value);
-//        System.out.println("pulse on 50hz " + (value/1000));
-//        Gpio.pwmWrite(26, value);
-////        pin
-////        pin0.setPwm((int)Math.round(1024*powerContainer.getLeftBackEnginePower()));
+    @Override
+    public void setEnginesPower(QuadEnginePowerContainer powerContainer) {
+        this.leftFrontPin.setPwm((int) Math.round(50 * powerContainer.getLeftFrontEnginePower() + 50));
+        this.leftBackPin.setPwm((int) Math.round(50 * powerContainer.getLeftBackEnginePower() + 50));
+        this.rightBackPin.setPwm((int) Math.round(50 * powerContainer.getRightBackEnginePower() + 50));
+        this.rightFrontPin.setPwm((int) Math.round(50 * powerContainer.getRightFrontEnginePower() + 50));
     }
 
     @Override
     public void close() {
         GPIO.shutdown();
-        //todo release
-//        GPIO.unprovisionPin(pin0);
+        GPIO.unprovisionPin(leftBackPin);
+        GPIO.unprovisionPin(leftFrontPin);
+        GPIO.unprovisionPin(rightBackPin);
+        GPIO.unprovisionPin(rightFrontPin);
     }
 }
