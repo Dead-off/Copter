96bef64c86d9cc3346717426d3c1c0f5fe14a706
Dead_off <AnterKan1@gmail.com>
2017-03-26 14:25:14 +0300

    test mock

--- src/test/java/engine/GPIOQuadCopterControllerTest.java
+++ src/test/java/engine/GPIOQuadCopterControllerTest.java
@@ -1,5 +1,8 @@
 package engine;
 
+import com.pi4j.io.gpio.GpioPinPwmOutput;
+import com.pi4j.io.gpio.impl.GpioControllerImpl;
+import com.pi4j.io.gpio.impl.GpioPinImpl;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
